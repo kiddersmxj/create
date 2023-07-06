44edc94abd9b4e44992ab8584da87c60e0e1b025
kiddersmxj <maxkiddmay@gmail.com>
2023-07-06 18:25:06 +0100

    setup add function for full useability with cpp related fs

--- src/create.cpp
+++ src/create.cpp
@@ -35,12 +35,12 @@ int main(int argc, char** argv) {
                 Usage();;
                 return EXIT_FAILURE;
             }
-            if(CreateFlag || AddFlag) {
-                if(Type == "" || Name == "") {
-                    Usage("Requires type and name to be passed");
-                    return EXIT_FAILURE;
-                }
-            }
+            /* if(CreateFlag || AddFlag) { */
+            /*     if(Type == "" || Name == "") { */
+            /*         Usage("Requires type and name to be passed"); */
+            /*         return EXIT_FAILURE; */
+            /*     } */
+            /* } */
             break;
         } 
 
@@ -59,6 +59,9 @@ int main(int argc, char** argv) {
             break;
         case 'c':
             CreateFlag = 1;
+            break;
+        case 'a':
+            AddFlag = 1;
             break;
         case '?':
             /* A return value of '?' indicates that an option was malformed.
@@ -89,12 +92,10 @@ int main(int argc, char** argv) {
         }
     }
     if(AddFlag) {
-        if(Type == "cpp") {
-            Add(Name, 0, 0);
-        }
+        Add(Name, Type, 0);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void Usage() {
@@ -124,46 +125,85 @@ void BuildFS(std::string Name, std::string Type) {
     Add("", "gdb", 0);
     Add("", "gitignore", 0);
     Add("", "license", 0);
-    k::ExecCmd("git init" + Sh + "; git branch -m main" + Sh \
-            + Sh + "; shopt -s dotglob" + Sh + "; git add *" + Sh + \
-            "; git remote add master " + BaseURL + "/" + Name + \
-            Sh + "; git commit -am \"initial commit\"" + Sh + "; git pull"\
-            + Sh + "; git branch  --set-upstream-to=master/main main" + Sh + \
-            "git push --set-upstream master main" + Sh, 0, 0);
+    CreateRepo(Name);
 }
 
 void Add(std::string Name, std::string Type, bool Main) {
+    std::cout << "e\n";
     if(Type == "cpp") {
         k::MkDir("inc");
         k::MkDir("src");
         if(Name == "config") { // config file
-        k::WriteFileLines("#ifndef Kconfig\n#define Kconfig\
-                            \n\n#include <iostream>\n\n#endif" \
-                            + LicenseFooterSlash, "inc/config.hpp");
+            k::WriteFileLines("#ifndef Kconfig\n#define Kconfig\
+                                \n\n#include <iostream>\n\n#endif" \
+                                + LicenseFooterSlash, "inc/config.hpp");
+            Git("add inc/config.hpp");
+        } else {
+            if(Main) { // TODO add classes/stucts
+                k::WriteFileLines("#include \"../inc/" + Name + ".hpp\"\n\n \
+                                    int main(int argc, char** argv) { \n\treturn 0;\n}" \
+                                    + LicenseFooterSlash, "src/" + Name + ".cpp");
+                k::WriteFileLines("#ifndef K" + Name + Name + "\n#define K" + Name + Name \
+                                    + "\n\n#include \"config.hpp\"\n\n#endif" \
+                                    + LicenseFooterSlash, "inc/" + Name + ".hpp");
+            } else {
+                k::WriteFileLines("#include \"../inc/" + Name + ".hpp\"\n" \
+                                    + LicenseFooterSlash, "src/" + Name + ".cpp");
+                k::WriteFileLines("#ifndef K" + Name + Name + "\n#define K" + Name + Name \
+                                    + "\n\n#endif" + LicenseFooterSlash, "inc/" + Name + ".hpp");
+            }
+            Git("add src/" + Name + ".cpp");
+            Git("add inc/" + Name + ".hpp");
         }
-        k::WriteFileLines("#include \"../inc/" + Name + ".hpp\"\n\n \
-                            int main(int argc, char** argv) { \n\treturn 0;\n}" \
-                            + LicenseFooterSlash, "src/" + Name + ".cpp");
-        k::WriteFileLines("#ifndef K" + Name + Name + "\n#define K" + Name + Name \
-                            + "\n\n#include \"config.hpp\"\n\n#endif" \
-                            + LicenseFooterSlash, "inc/" + Name + ".hpp");
     } else if(Type == "cmake") {
         k::WriteFileLines(Install + LicenseFooterHash, "install.sh");
-        k::ExecCmd("chmod +x install.sh", 0, 0);
+        int result = k::ExecCmd("chmod +x install.sh");
         k::WriteFileLines(CMake1 + Name + CMake2 + Name + " src/" \
                             + Name + ".cpp" + CMake3 + Name + CMake4 + Name + CMake5 \
                             + Name + CMake6 + LicenseFooterHash, "CMakeLists.txt");
         k::MkDir(".github");
         k::MkDir(".github/workflows");
         k::WriteFileLines(Action, ".github/workflows/cmake.yml");
+        Git("add CMakeLists.txt");
+        Git("add .github");
     } else if(Type == "license") {
         k::WriteFileLines(License, "LICENSE");
+        Git("add LICENSE");
     } else if(Type == "gdb") {
         k::WriteFileLines(GDB, ".gdbinit");
+        Git("add .gdbinit");
     } else if(Type == "gitignore") {
         k::WriteFileLines(GitIgnore, ".gitignore");
+        Git("add .gitignore");
     } else if(Type == "readme") {
         std::string ReadMe = "#" + Name + "\n\n" + ReadMe1 + Name + ReadMe2 + Name + ReadMe3;
         k::WriteFileLines(ReadMe, "README.md");
+        Git("add README.md");
+    } else if(Type == "bash" || Type == "sh") {
+        std::string Bash = "#!/bin/bash\n";
+        k::WriteFileLines(Bash, Name + ".sh");
+        int result = k::ExecCmd("chmod +x " + Name + ".sh");
+        Git("add" + Name + ".sh");
     }
+}
+
+bool Git(std::string Cmd) {
+    Cmd =  "git " + Cmd;
+    std::cout << "cmd: " << Cmd << std::endl;
+    std::string Output;
+    int result = k::ExecCmd(Cmd, Output);
+    std::cout << "0: " << Output << std::endl;
+    return result;
+}
+
+bool CreateRepo(std::string Name) {
+    Git("init");
+    Git("branch -m main");
+    Git("remote add master " + BaseURL + "/" + Name);
+    Git("add *; shopt -s dotglob; git add *");
+    Git("commit -am \"intial commit\"");
+    Git("pull");
+    Git("branch  --set-upstream-to=master/main main");
+    Git("push --set-upstream master main");
+    return 0;
 }
